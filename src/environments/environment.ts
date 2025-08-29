53256a1316e51483a4203ee640d59316502fbd48
Charishma200308 <sbandaru@ctepl.com>
2025-08-29 11:29:16 +0530

    chore: URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const Environment: any = {
-    URI: 'https://resourcetrackerapp-c5bxfwdhe5bnfvfs.canadacentral-01.azurewebsites.net/EmployeeDetails/',
-    URL: 'https://resourcetrackerapp-c5bxfwdhe5bnfvfs.canadacentral-01.azurewebsites.net/api/Auth/',
+    // URI: 'https://resourcetrackerapp-c5bxfwdhe5bnfvfs.canadacentral-01.azurewebsites.net/EmployeeDetails/',
+    // URL: 'https://resourcetrackerapp-c5bxfwdhe5bnfvfs.canadacentral-01.azurewebsites.net/api/Auth/',
 
-    // URI: 'https://localhost:7203/EmployeeDetails/',
-    // URL: 'https://localhost:7203/api/Auth/',
+    URI: 'https://localhost:7203/EmployeeDetails/',
+    URL: 'https://localhost:7203/api/Auth/',
 }
